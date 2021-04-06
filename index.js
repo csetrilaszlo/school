4f651ddca7c39365d8f1e54c344a511d71e9e4fe
Csetri Laszlo <csetrilaszlo@gmail.com>
2021-04-06 15:55:52 +0300

    commit1

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ console.log("JavaScript - Cookies & LocalStorage");
 
 console.log(document.cookie);
 
+if (!document.cookie) {
+  window.location = "/login.html";
+}
 
+if (document.cookie) {
+  const cookiesElements = document.cookie.split("; ");
+  console.log(cookiesElements);
+  const cookieObj = {};
+  for (const element of cookiesElements) {
+    const [key, value] = element.split("=");
+    cookieObj[key] = value;
+    console.log(key, value);
+  }
+  console.log(cookieObj);
 
-if (!localStorage.getItem("name") || (!localStorage.getItem("password"))) {
- 
+  if (!cookieObj.name || !cookieObj.password) {
     window.location = "/login.html";
-  
+  }
 }
 
 document.getElementById("logout").addEventListener("click", () => {
-  localStorage.removeItem('name');
-  localStorage.removeItem('password');
+  document.cookie = "password=123; ; Expires=Thu, 31 Oct 1990 07:28:00 GMT;";
+  document.cookie = "name=123; ; Expires=Thu, 31 Oct 1990 07:28:00 GMT;";
   window.location = "/";
 });
-
