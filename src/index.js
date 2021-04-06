559a895d650ccd65c97e70a3808f7a7a4337947a
Csetri Laszlo <csetrilaszlo@gmail.com>
2021-04-06 21:04:00 +0300

    commit1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log("Entry point to webpack");
+import { addPixel, addPercentage } from "./utils/addUnitsType";
+
+const aRandomNumber = Math.floor(Math.random() * 20);
+console.log(aRandomNumber);
+
+const aRandomNumberWithPixel = addPixel(aRandomNumber);
+console.log(aRandomNumberWithPixel);
+
+const aRandomNumberWithPercentage = addPercentage(aRandomNumber);
+console.log(aRandomNumberWithPercentage);
---
+++ src/utils/addUnitsType.js
@@ -0,0 +1,8 @@
+export function addPixel(value) {
+    return `${value}px`;
+  }
+  
+  export function addPercentage(value) {
+    return `${value}%`;
+  }
+  
