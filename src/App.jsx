e9bfc1847df73747d4afac7fd2115bd4310040b9
Csetri Laszlo <csetrilaszlo@gmail.com>
2021-04-20 20:58:56 +0300

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Header } from "./components/Header/Header";
 import { ToDoItem } from "./components/ToDoItem/ToDoItem";
 
 import './App.css';
+import { ToDoItemWithState } from "./components/ToDoItemWithState./ToDoItemWithState";
 
 
 function App() {
   return (
     <div className="App" id="app">
       <Header />
-      <ToDoItem />
+      <ToDoItem label="task1" checkValue={false}/>
+      <ToDoItem label="task2" checkValue={true}/>
+      <ToDoItem label="task3" />
+      <ToDoItemWithState label="task 4" checkValue={true}/>
     </div>
   );
 }
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,9 +1,11 @@
 import './ToDoItem.css';
 
-export function ToDoItem(){
+export function ToDoItem(props){
+    console.log(props);
+
     return (<div className="to-do-item">
-        <input type="checkbox"/>
-        <p>Item</p>
+        <input type="checkbox" defaultChecked={props.checkValue}/>
+        <p>{props.label}</p>
         <img src="https://www.flaticon.com/svg/vstatic/svg/860/860829.svg?token=exp=1618938216~hmac=3f7e367554afc12a493cbac6db91d874" 
         alt="trash"/>
     </div>)
